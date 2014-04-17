37833f343f49168a88bcd992806c9e1328fa41e4
Trevor Harwell <trevor.harwell@evisions.com>
2014-04-17 12:24:04 -0700

    Adding qunit and require setup

---
+++ requireconfig.js
@@ -0,0 +1,8 @@
+var require = {
+  paths: {
+    backbone   : 'libs/backbone/backbone',
+    jquery     : 'libs/jquery/dist/jquery',
+    qunit      : 'libs/qunit/qunit/qunit',
+    underscore : 'libs/underscore/underscore'
+  }
+};
